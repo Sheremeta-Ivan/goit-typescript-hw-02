156729c0a0100e612280c3c86186f20bfb727a3a
Ivan Sheremeta <sheremetavanya2004@gmail.com>
2023-10-07 10:12:36 +0300

    task 4

--- src/baseTypes/4.ts
+++ src/baseTypes/4.ts
@@ -3,6 +3,16 @@
   щоб гарантувати, що перший елемент завжди буде рядком, а другий числом?
 */
 
-let person = ['Max', 21];
+let person: [string, number];
+person = ["Max", 21];
+
+//or
+// let person: [string, number] = ["Max", 21];
+
+//or
+// type Person = [string, number];
+// let person: Person = ["Max", 21];
+
+// Вкажете в коментарі до ДЗ чи всі 3 способа правильні?
 
 export {};
